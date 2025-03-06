1eee099697c8d6a4a6fb613367c843a0a58648e1
Pastamaster74 <aku.playstation@gmail.com>
2025-03-06 16:33:56 +0200

    improved ui

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.*;
 
 public class App {
@@ -12,14 +13,14 @@ public class App {
 
         JLabel topBar = new JLabel();
         topBar.setBounds(0, 0, 800, 15);
-        topBar.setOpaque(false);
+        topBar.setOpaque(true);
         topBar.setBackground(Color.GRAY);
 
         JTextArea textArea = new JTextArea();
         textArea.setBounds(45, 25, 700, 250);
+        textArea.setFont(new Font("sans-serif", Font.PLAIN, 13));
         textArea.setForeground(Color.WHITE);
-        textArea.setOpaque(true);
-        textArea.setBorder(null);
+        textArea.setOpaque(false);
 
         mainFrame.add(topBar);
         mainFrame.add(textArea);
