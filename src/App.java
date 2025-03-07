52e45ad1e5faff87f5c734ec4dbad1ef460c753e
Pastamaster74 <aku.playstation@gmail.com>
2025-03-07 13:55:12 +0200

    more ui improvements

--- src/App.java
+++ src/App.java
@@ -12,18 +12,32 @@ public class App {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel topBar = new JLabel();
-        topBar.setBounds(0, 0, 800, 15);
+        topBar.setBounds(0, 0, 800, 20);
         topBar.setOpaque(true);
         topBar.setBackground(Color.GRAY);
 
         JTextArea textArea = new JTextArea();
-        textArea.setBounds(45, 25, 700, 250);
+        textArea.setBounds(25, 30, 750, 525);
         textArea.setFont(new Font("sans-serif", Font.PLAIN, 13));
+        textArea.setBackground(new Color(40, 149, 168));
         textArea.setForeground(Color.WHITE);
-        textArea.setOpaque(false);
+        textArea.setBorder(BorderFactory.createSoftBevelBorder(1, Color.GRAY, Color.gray));
+        textArea.setOpaque(true);
+
+        JLabel cornerLabel1 = new JLabel();
+        cornerLabel1.setBounds(700, 20, 100, 75);
+        cornerLabel1.setOpaque(true);
+        cornerLabel1.setBackground(Color.WHITE);
+
+        JLabel cornerLabel2 = new JLabel();
+        cornerLabel2.setBounds(15, 475, 100, 100);
+        cornerLabel2.setOpaque(true);
+        cornerLabel2.setBackground(Color.WHITE);
 
         mainFrame.add(topBar);
         mainFrame.add(textArea);
+        mainFrame.add(cornerLabel1);
+        mainFrame.add(cornerLabel2);
         mainFrame.repaint();
     }
 }
